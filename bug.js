f8f79cfbada3139f0d8b821dbd5b63514b7ac03d
BugHunter-X <askzenai@gmail.com>
2025-01-26 23:52:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,22 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This effect will run after every render
+    console.log('Count:', count); 
+    // This is problematic if count is updated frequently
+    return () => {
+      // Cleanup function
+      console.log('Cleanup');
+    };
+  }, [count]);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+```
